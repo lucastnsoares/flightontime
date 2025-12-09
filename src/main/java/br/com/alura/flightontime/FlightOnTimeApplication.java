85cc6e1a931b7720ea0fdb15d1ad5181c13f512b
Lucas Soares <lucastnogsoares@gmail.com>
2025-12-08 23:51:27 -0300

    Commit inicial

---
+++ src/main/java/br/com/alura/flightontime/FlightOnTimeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.flightontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightOnTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightOnTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/flightontime/FlightOnTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.flightontime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightOnTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
